7c3736783a8fecf989cc6311247c90351f42e679
Vova Shliakhovyi <vovashliakhovyi@Jennifers-MacBook-Pro.local>
2020-06-09 20:01:36 -0600

    Integration functions

--- javascript/tests/02_integrationFunctions/integrationFunctions.test.js
+++ javascript/tests/02_integrationFunctions/integrationFunctions.test.js
@@ -1,6 +1,20 @@
 import axios from 'axios';
 
 /**
+ *
+ * @param {Str} url
+ * @param {Str} method
+ * @param {Object} config
+ * @description Helper fetch function used to create integration functions
+ */
+export const fetch = (url, method, config) =>
+	axios({
+		url,
+		method,
+		...config // Feel free to reference the axios docs for what this config might consist of (headers, params, etc)..
+	});
+
+/**
  * Write a function(s) that takes a config object defining a list of urls with their available http methods
  * and returns an object that has the available api functions to interact with each of those endpoints.
  * This problem relies using on axios. We provided a helper `fetch` function to abstract axios for you.
@@ -9,94 +23,115 @@ import axios from 'axios';
  * @param {String} prefix
  * @returns {Object}
  */
-export const integrationFunctions = integrationConfig => { // eslint-disable-line no-unused-vars
-    // Write your solution here
+export const integrationFunctions = integrationConfig => {
+	// eslint-disable-line no-unused-vars
+	// Write your solution here
+	if (!integrationConfig) {
+		return;
+	}
+	const apiFunctions = {};
+
+	for (const key in integrationConfig) {
+		let methods = integrationConfig[key].methods;
+		let url = integrationConfig[key].url;
+
+		apiFunctions[key] = {};
+
+		methods.forEach(method => {
+			apiFunctions[key][method] = (id, config) => {
+				const urlWithParams = new URL(url);
+				const param = urlWithParams.pathname.match(/.*:(.*)\//);
+				if (param) {
+					const path = urlWithParams.pathname.match(/(.*):/)[1];
+					url = `${urlWithParams.origin}${path}${id.id}/`;
+				}
+
+				return fetch(url, method, config);
+			};
+		});
+	}
+
+	console.log(apiFunctions);
+
+	return apiFunctions;
 };
 
-/**
- *
- * @param {Str} url
- * @param {Str} method
- * @param {Object} config
- * @description Helper fetch function used to create integration functions
- */
-export const fetch = (url, method, config) => axios({
-    url,
-    method,
-    ...config, // Feel free to reference the axios docs for what this config might consist of (headers, params, etc)..
-});
-
 export const routeDefinitions = {
-    track: {
-        methods: ['get', 'patch', 'delete'],
-        url: 'https://api.testing.com/tracks/:id/'
-    },
-    tracks: {
-        methods: ['get', 'post'],
-        url: 'https://api.testing.com/tracks/'
-    },
+	track: {
+		methods: ['get', 'patch', 'delete'],
+		url: 'https://api.testing.com/tracks/:id/'
+	},
+	tracks: {
+		methods: ['get', 'post'],
+		url: 'https://api.testing.com/tracks/'
+	}
 };
 
 export const expected = {
-    track: {
-        get: (id, config) => axios({
-            url: `https://api.testing.com/tracks/${id}/`,
-            method: 'get',
-            ...config,
-        }),
-        patch: (id, config) => axios({
-            url: `https://api.testing.com/tracks/${id}/`,
-            method: 'patch',
-            ...config,
-        }),
-        delete: (id, config) => axios({
-            url: `https://api.testing.com/tracks/${id}/`,
-            method: 'delete',
-            ...config,
-        }),
-    },
-    tracks: {
-        get: config => axios({
-            url: 'https://api.testing.com/tracks/',
-            method: 'get',
-            ...config,
-        }),
-        post: config => axios({
-            url: 'https://api.testing.com/tracks/',
-            method: 'post',
-            ...config,
-        }),
-    }
+	track: {
+		get: (id, config) =>
+			axios({
+				url: `https://api.testing.com/tracks/${id}/`,
+				method: 'get',
+				...config
+			}),
+		patch: (id, config) =>
+			axios({
+				url: `https://api.testing.com/tracks/${id}/`,
+				method: 'patch',
+				...config
+			}),
+		delete: (id, config) =>
+			axios({
+				url: `https://api.testing.com/tracks/${id}/`,
+				method: 'delete',
+				...config
+			})
+	},
+	tracks: {
+		get: config =>
+			axios({
+				url: 'https://api.testing.com/tracks/',
+				method: 'get',
+				...config
+			}),
+		post: config =>
+			axios({
+				url: 'https://api.testing.com/tracks/',
+				method: 'post',
+				...config
+			})
+	}
 };
 
 jest.mock('axios');
 
 describe('integrationFunctions', () => {
-    beforeEach(() => {
-        axios.mockReset();
-    });
+	beforeEach(() => {
+		axios.mockReset();
+	});
 
-    const api = integrationFunctions(routeDefinitions);
+	const api = integrationFunctions(routeDefinitions);
 
-    it('creates an object of api integration functions based on user supplied config', () => {
-        expect(Object.keys(api)).toEqual(['track', 'tracks']);
-        expect(Object.keys(api.track)).toEqual(['get', 'patch', 'delete']);
-        expect(Object.keys(api.tracks)).toEqual(['get', 'post']);
-    });
+	it('creates an object of api integration functions based on user supplied config', () => {
+		expect(Object.keys(api)).toEqual(['track', 'tracks']);
+		expect(Object.keys(api.track)).toEqual(['get', 'patch', 'delete']);
+		expect(Object.keys(api.tracks)).toEqual(['get', 'post']);
+	});
 
-    it('calls an api endpoints without url params', () => {
-        api.tracks.get();
-        expect(axios).toHaveBeenCalledWith({
-            method: 'get',
-            url: 'https://api.testing.com/tracks/'
-        });
-    });
+	it('calls an api endpoints without url params', () => {
+		api.tracks.get();
+		expect(axios).toHaveBeenCalledWith({
+			method: 'get',
+			url: 'https://api.testing.com/tracks/'
+		});
+	});
 
-    it('calls an api endpoints with url params', () => {
-        api.track.get({ id: 123 });
-        expect(axios).toHaveBeenCalledWith({
-            method: 'get',
-            url: 'https://api.testing.com/tracks/123/'
-        });
-    });
+	it('calls an api endpoints with url params', () => {
+		api.track.get({ id: 123 });
+		expect(axios).toHaveBeenCalledWith({
+			method: 'get',
+			url: 'https://api.testing.com/tracks/123/'
+		});
+	});
 });
